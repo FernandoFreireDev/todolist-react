f98fe40603d4c93f69caf3a4c02b36761558a7df
Fernando Freire <fernando.freire@bemobi.com.br>
2020-01-20 23:46:22 -0300

    removendo item do estado

--- src/components/TechList.js
+++ src/components/TechList.js
@@ -23,11 +23,20 @@ class TechList extends Component {
     });
   }
 
+  handleDelete = (tech) => {
+    this.setState({ techs: this.state.techs.filter(t => t !== tech) });
+  }
+
   render() {
     return (
       <form onSubmit={this.handleSubmit}>
         <ul>
-          {this.state.techs.map((tech, index) => <li key={index}>{tech}</li>)}
+          {this.state.techs.map((tech, index) => (
+            <li key={index}>
+              {tech}
+              <button type="button" onClick={() => this.handleDelete(tech)}>Remover</button>
+            </li>
+          ))}
         </ul>
         <input 
           type="text" 
